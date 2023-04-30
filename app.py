3cb7e2db01288043732e279b1380fcb881bca28f
Almog Arazi <112971847+Almog-Arazi@users.noreply.github.com>
2023-04-30 17:31:45 +0300

    update my code for use selenuium
    update my code for use selenuium
    
    works in non headless mode

--- app.py
+++ app.py
@@ -3,92 +3,129 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from bs4 import BeautifulSoup
-import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+import os
 import time
-
+import re
+from typing import Optional
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-def get_headers():
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Accept-Language": "en-US,en;q=0.9",
-        "Upgrade-Insecure-Requests": "1",
-        "Connection": "keep-alive",
-        "DNT": "1"
-    }
-    return headers
+
+
+def fetch_page_content(url):
+    chrome_options = Options()
+   # chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--remote-debugging-port=9222")
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--disable-infobars")
+    chrome_options.add_argument("--disable-popup-blocking")
+    
+    driver = webdriver.Chrome(executable_path='/Users/almog/chromedriver/chromedriver', options=chrome_options)
+    driver.get(url)
+    
+    try:
+        WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.ID, "search")))
+    except TimeoutException:
+        print("Element not found or it took too long to load the page.")
+        driver.quit()
+        return None
+
+    page_source = driver.page_source
+    driver.quit()
+    return page_source
+
+
 
 def build_url(search_query):
     search_query = search_query.replace(' ', '+')
     base_url = 'https://www.amazon.com/s?k={0}'.format(search_query)
     return base_url
+    
+def extract_product_info(result) -> Optional[dict]:
+    try:
+        title = result.h2.a.text.strip()
+    except AttributeError:
+        title = None
 
-def extract_product_info(result):
-    asin = result['data-asin']
-    product_name = result.h2.text
+    try:
+        product_url = 'https://amazon.com' + result.h2.a['href']
+    except (AttributeError, TypeError):
+        product_url = None
 
     try:
         img_url = result.find('img', {'class': 's-image'})['src']
-    except AttributeError:
-        img_url = ""
+    except (AttributeError, TypeError):
+        img_url = None
 
     try:
-        rating = result.find('i', {'class': 'a-icon'}).text
-        rating_count = result.find_all('span', {'aria-label': True})[1].text
-    except AttributeError:
-        return None
+        rating = float(result.find('span', {'class': 'a-icon-alt'}).text.split()[0])
+    except (AttributeError, ValueError, IndexError):
+        rating = None
 
     try:
-        price1 = result.find('span', {'class': 'a-price-whole'}).text
-        price2 = result.find('span', {'class': 'a-price-fraction'}).text
-        price = float((price1 + price2).replace(',', ''))
-        product_url = 'https://amazon.com' + result.h2.a['href']
-    except AttributeError:
-        return None
-
-    return product_name, rating, rating_count, price, product_url, img_url, asin
-
-def scrape_de_price(asin, product_name):
-    headers = get_headers()
-    base_url = f'https://www.amazon.de/dp/{asin}'
-    response = requests.get(base_url, headers=headers)
-    soup = BeautifulSoup(response.content, 'html.parser')
-
-    if "404 - Datei oder Verzeichnis wurde nicht gefunden" in str(soup):
-        search_query = product_name.replace(' ', '+')
-        base_url = f'https://www.amazon.de/s?k={search_query}'
-        response = requests.get(base_url, headers=headers)
-        soup = BeautifulSoup(response.content, 'html.parser')
+        review_count = int(result.find('span', {'class': 'a-size-base'}).text.replace(',', ''))
+    except (AttributeError, ValueError):
+        review_count = None
 
     try:
-        price1 = soup.find('span', {'class': 'a-price-whole'}).text
-        price2 = soup.find('span', {'class': 'a-price-fraction'}).text
-        price = float((price1 + price2).replace(',', '.'))
+        price = result.select_one("span.a-price span.a-offscreen").text
     except AttributeError:
         price = None
 
-    return price
+    if not price:
+        try:
+            price = result.select_one("span.a-price-range span.a-offscreen").text
+        except AttributeError:
+            price = None
+
+    if not price:
+        try:
+            price = result.select_one("span.a-text-price span.a-offscreen").text
+        except AttributeError:
+            price = None
+
+    # Add the following lines to extract ASIN
+    try:
+        asin = result.h2.a['href'].split('/')[3]
+    except (AttributeError, TypeError, IndexError):
+        asin = None
+
+    if not title:
+        return None
+
+    product_info = {
+        'title': title,
+        'product_url': product_url,
+        'img_url': img_url,
+        'rating': rating,
+        'review_count': review_count,
+        'price': price,
+        'asin': asin  # Add the 'asin' field to the product_info dictionary
+    }
+    return product_info
 
 
 def scrape_top_10_products(search_query):
-    headers = get_headers()
     items = []
     item_count = 0
 
     url = build_url(search_query)
     print('Processing {0}...'.format(url))
-    response = requests.get(url, headers=headers)
-    time.sleep(5)  # Add a delay of 5 seconds between requests
-    print('Status code:', response.status_code)  # Add this line to print the status code
-    soup = BeautifulSoup(response.content, 'html.parser')
+    page_content = fetch_page_content(url)
+    soup = BeautifulSoup(page_content, 'html.parser')
 
-    results = soup.find_all('div', {'class': 's-result-item', 'data-component-type': 's-search-result'})
+    results = soup.select("div[data-index]")
 
     for result in results:
         if item_count >= 10:
@@ -101,29 +138,26 @@ def scrape_top_10_products(search_query):
 
     return items
 
+
 @app.get("/", response_class=HTMLResponse)
 async def search(request: Request):
     img_url = "/static/logo.png"
     return templates.TemplateResponse("search.html", {"request": request, "img_url": img_url})
-
 
 @app.post("/result", response_class=HTMLResponse)
 async def result(request: Request, search_query: str = Form(...)):
     items = scrape_top_10_products(search_query)
     return templates.TemplateResponse("result.html", {"request": request, "items": items, "enumerate": enumerate, "search_query": search_query})
 
+
 @app.post("/product", response_class=HTMLResponse)
 async def product(request: Request, asin: str = Form(...), search_query: str = Form(...)):
     items = scrape_top_10_products(search_query)
     for item in items:
-        if item[6] == asin:
-            product_name = item[0]
-            img_url = item[5]
-            price = item[3]
+        if item['asin'] == asin:  # Use the 'asin' key instead of the index
+            product_name = item['title']  # Use the 'title' key instead of the index
+            img_url = item['img_url']  # Use the 'img_url' key instead of the index
+            price = item['price']  # Use the 'price' key instead of the index
             break
 
-    de_price = scrape_de_price(asin, product_name)
-
-    return templates.TemplateResponse("product.html", {"request": request, "product_name": product_name, "img_url": img_url, "price": price, "asin": asin, "de_price": de_price})
-
-
+    return templates.TemplateResponse("product.html", {"request": request, "product_name": product_name, "img_url": img_url, "price": price, "asin": asin})
